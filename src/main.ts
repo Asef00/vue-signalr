ddc8512ef4af19c318a2b7c73b3f9f725132a728
Giga <gorgadzegiga1999@gmail.com>
2023-09-21 22:04:26 +0400

    remove unnecassry code

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import * as signalR from '@microsoft/signalr'
-
-const connection = new signalR.HubConnectionBuilder()
-.withUrl("http://192.168.1.100:8080/hub")
-.build();
-
-connection.on("NumberOfOnlineUsers", data => {
-  console.log(data);
-});
-
-connection.start()
-    // .then(() => connection.invoke("NumberOfOnlineUsers"));
-
 const app = createApp(App)
 
 app.use(createPinia())
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import * as signalR from '@microsoft/signalr'
+
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl('http://192.168.1.100:8080/hub')
+  .build()
+
+connection.on('NumberOfOnlineUsers', (data) => {
+  console.log(data)
+})
+
+connection.start()
+// .then(() => connection.invoke("NumberOfOnlineUsers"));
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
